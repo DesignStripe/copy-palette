83fa885c1d5d0f3056da8d842f85de5afe6c4176
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-08-02 21:50:42 +0200

    Robust faqs solution

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import faqs from "../utils/faqs";
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -30,47 +32,33 @@ const Container = styled.div`
   }
 `;
 
+const FAQContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-bottom: 2rem;
+
+  & > *:first-child {
+    margin-bottom: 1rem;
+  }
+`;
+
+const FAQ = ({ question, answer }) => {
+  return (
+    <FAQContainer>
+      <h2>{question}</h2>
+      <p>{answer}</p>
+    </FAQContainer>
+  );
+};
+
 const About = () => (
   <Container>
     <h1>About</h1>
-    <h2>What is CopyPalette</h2>
-    <p>
-      CopyPalette was build for User Interface designers that struggle (or just
-      feel bored) to generate a consistent monochromatic color palette. By
-      fine-tuning some parameters you can effortlessly create different shades
-      and depths of a single hue and export your palette in a single click!
-    </p>
-    <h2>How does CopyPalette work?</h2>
-    <p>
-      CopyPalette app is pretty straightforward! It works as simple as that.
-      There are 3 main parameters: the base color, the contrast, variations.
-    </p>
-    <h2>Which parameters are available? </h2>
-    <p>
-      Monochromatic palettes can be boring when done poorly. Thus, CopyPalette
-      provides 3 parameters to skyrocket your palette:
-    </p>
-    <ol>
-      <li>
-        <h3>Base color</h3>
-        <p>The basic hue to create</p>
-      </li>
-      <li>
-        <h3>Contrast</h3>
-        <p>The basic hue to create</p>
-      </li>
-      <li>
-        <h3>Variations</h3>
-        <p>The basic hue to create</p>
-      </li>
-    </ol>
-    <h2>Who created CopyPalette? </h2>
-    <p>
-      Woddd was created as a fun side project by Dimitris Raptis who used to
-      search for hours and kill wods every Sunday. Dimitris is the co-founder of
-      Loceye and a fitness freak. User Interface design and Front-end
-      development are his geek interests.
-    </p>
+    {faqs.map((faq, index) => (
+      <FAQ key={index} question={faq.question} answer={faq.answer} />
+    ))}
   </Container>
 );
 
---
+++ src/utils/faqs.js
@@ -0,0 +1,24 @@
+const faqs = [
+  {
+    question: "What is CopyPalette?",
+    answer:
+      "CopyPalette was build for User Interface designers that struggle (or just feel bored) to generate a consistent monochromatic color palette. By fine-tuning some parameters you can effortlessly create different shades and depths of a single hue and export your palette in a single click!"
+  },
+  {
+    question: "How does CopyPalette work?",
+    answer:
+      "CCopyPalette app is pretty straightforward! It works as simple as that.There are 3 main parameters: the base color, the contrast, variations."
+  },
+  {
+    question: "Which parameters are available?",
+    answer:
+      "Monochromatic palettes can be boring when done poorly. Thus, CopyPalette provides 3 parameters to skyrocket your palette: Base color The basic hue to create"
+  },
+  {
+    question: "Who created CopyPalette?",
+    answer:
+      "Woddd was created as a fun side project by Dimitris Raptis who used to search for hours and kill wods every Sunday. Dimitris is the co-founder of Loceye and a fitness freak. User Interface design and Front-end development are his geek interests."
+  }
+];
+
+export default faqs;
