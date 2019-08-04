9e97ba146925c56f30fd71bf7d0715f98207d18a
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-08-04 20:48:05 +0200

    Add Item component and icons

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiThermometer, FiGift, FiInfo } from "react-icons/fi";
 import Popper from "@material-ui/core/Popper";
 
 import Link from "../components/Link";
@@ -29,19 +29,34 @@ const Dropdown = styled.div`
   background-color: ${props => props.theme.colors.primary[500]};
   color: ${props => props.theme.colors.primary[100]};
 
-  & > div > a {
-    color: ${props => props.theme.colors.primary[100]};
-  }
   & > div {
     margin-bottom: 1rem;
-    cursor: pointer;
   }
   & > div:last-child {
     margin-bottom: 0;
   }
-  & > div:hover,
-  & > div > a:hover {
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  cursor: pointer;
+
+  &:hover,
+  & > a:hover {
     color: ${props => props.color};
+  }
+  & > a {
+    color: ${props => props.theme.colors.primary[100]};
+  }
+  & > * {
+    margin-right: 0.5rem;
+  }
+  & > *:last-child {
+    margin-right: 0;
   }
 `;
 
@@ -60,11 +75,15 @@ const Settings = ({ handleTheme, handleType, color }) => {
 
       <FiSettings onClick={handleClick} size={20} />
       <Popper id={id} open={open} anchorEl={anchorEl}>
-        <Dropdown color={color}>
-          <div onClick={handleType}>Toggle Palette Preview</div>
-          <div>
+        <Dropdown>
+          <Item color={color} onClick={handleType}>
+            <FiGift />
+            <p>Toggle Palette Preview</p>
+          </Item>
+          <Item color={color}>
+            <FiInfo />
             <Link to="/about">About</Link>
-          </div>
+          </Item>
         </Dropdown>
       </Popper>
     </Wrapper>
