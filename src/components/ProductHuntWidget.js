079a476d63b50970c200ab590e6443aa2e0212d1
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-09-21 10:05:22 +0300

    :ambulance: Add post id

--- src/components/ProductHuntWidget.js
+++ src/components/ProductHuntWidget.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const POST_ID = "167257";
+const POST_ID = "168516";
 
 export const ProducHuntWidget = ({ isDark }) => {
   return (
-    <a href="https://www.producthunt.com/posts/visualeyes-2" target="_blank">
+    <a href="https://www.producthunt.com/posts/copypalette-2" target="_blank">
       <img
         src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=${POST_ID}&theme=${
           isDark ? "light" : "dark"
